b80c19328e8375fa7b81842067cf632e830295c9
AshleyEvonne <ashleyevonne345@gmail.com>
2024-07-06 19:31:41 -0400

    Lowercase and Uppercase Method

---
+++ src/toLowerCase.java
@@ -0,0 +1,15 @@
+public class toLowerCase {
+    public static void main(String[] args) {
+        // The toLowerCase() method converts a string to lower case letters.
+
+        String s1 = "HELLO HOW Are You?";
+        String s1Lower = s1.toLowerCase();
+
+        System.out.println(s1Lower);
+
+        // The toUpperCase() method converts a string to upper case letters.
+        String s2 = "hello how are you?";
+        String s2Upper = s2.toUpperCase();
+        System.out.println(s2Upper);
+    }
+}
